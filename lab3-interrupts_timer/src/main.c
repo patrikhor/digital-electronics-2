2749d395c0151f178845933c47efa677076e9736
patrikhor <patrikhorcicka2000@gmail.com>
2022-10-12 14:42:29 +0200

    update

--- lab3-interrupts_timer/src/main.c
+++ lab3-interrupts_timer/src/main.c
@@ -15,7 +15,7 @@
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN PB5  // Arduino Uno on-board LED
 #define LED_RED PB0    // External active-low LED
-
+#define BUTTON PD2
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
@@ -36,12 +36,13 @@ int main(void)
     // Set pins where LEDs are connected as output
     GPIO_mode_output(&DDRB, LED_GREEN);
     GPIO_mode_output(&DDRB, LED_RED);
+    GPIO_mode_input_pullup(&DDRD, BUTTON);
 
     // Configuration of 16-bit Timer/Counter1 for LED blinking
     // Set the overflow prescaler to 262 ms and enable interrupt
     TIM1_overflow_262ms();
     TIM1_overflow_interrupt_enable();
-    TIM0_overflow_16ms();
+    TIM0_overflow_4ms();
     TIM0_overflow_interrupt_enable()
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -69,5 +70,8 @@ ISR(TIMER1_OVF_vect)
 }
 ISR(TIMER0_OVF_vect)
 {
-    PORTB = PORTB ^ (1<<LED_RED);
+    if(GPIO_read(&PIND, BUTTON)){
+        PORTB = PORTB ^ (1<<LED_RED);
+    }
+    
 }
